985ac347a02d619f2328f5ca9b07004ec6acede9
TheReaper62 <34223146+TheReaper62@users.noreply.github.com>
2022-04-15 22:06:58 +0800

    Proposed 1.9

--- gdrivepy/functions.py
+++ gdrivepy/functions.py
@@ -1,86 +1,126 @@
-from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
+import io
+
+from typing import Optional, Any
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def auth(credential_path):
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credential_path, SCOPES)
-            # creds = flow.run_local_server(port=0)
-            creds = flow.run_console()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+class File:
+    pass
 
-    service = build('drive', 'v3', credentials=creds)
-    return service
+class GDriver:
+    def __init__(self, credential_path:str)->None:
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(credential_path, SCOPES)
+                # creds = flow.run_local_server(port=0)
+                creds = flow.run_console()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-def listFiles(service):
-        # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    data = [];
-    if not items:
-        output = {}
-        output['status'] = 'error'
-        output['message']='No File Found'
-        data.append(output)
-        return data
-    else:
-        for item in items:
-            data.append(item)
-    return {
-    'status':'ok', 
-    'message':'File Found', 
-    'data':data
-    }
+        self.service = build('drive', 'v3', credentials=creds)
 
-def upload(service,new_name,file_path, mime, parents=False):
-    if parents == False:
-        file_metadata = {'name': new_name}
-    else:
-        file_metadata = { 'name' : new_name, 'parents' : parents }
-    media = MediaFileUpload(file_path, mime)
-    file = service.files().create(body=file_metadata,
-                                    media_body=media,
-                                    fields='id').execute()
-    return {
-    'status':'ok',
-    'message':'File Uploaded Successfully', 
-    'file_id':file.get('id')
-    }
+    def list_files(
+        self,
+        query:str="",
+        spaces:str="drive",
+        page_size:int=10,
+        fields:str="nextPageToken, files(id, name)"
+    )->list[File]:
+        page_token = None
+        files = []
+        while True:
+            args = {
+                "q" : query,
+                "spaces" : spaces,
+                "pageSize" : page_size,
+                "fields" : fields,
+            }
+            results = self.service.files().list(**args).execute()
+            items = results.get('files', [])
+            files.append(items)
 
-def createFolder(service,new_name, parents=False):
-    if parents == False:
-        file_metadata = {'name': new_name, 'mimeType' : 'application/vnd.google-apps.folder'}
-    else:
-        file_metadata = { 'name' : new_name, 'mimeType' : 'application/vnd.google-apps.folder', 'parents' : parents }
-    file = service.files().create(body=file_metadata,
-                                    fields='id').execute()
-    return {
-    'status':'ok',
-    'message':'File Uploaded Successfully', 
-    'file_id':file.get('id')
-    }
+            # Next Page
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+            return files
 
+    def upload(
+        self,
+        file_metadata:dict[str,Any],
+        file_path:str,
+        mime:str,
+        parents:bool=False,
+        resumable:bool=True,
+    )->Optional[int]:
+        '''
+        # For Google Sheets
+        metadata = {'name':'My Cool Report','mimeType':'application/vnd.google-apps.spreadsheet'}
+        GDriver.upload(file_metadata=metadata)
+        '''
+        media = MediaFileUpload(file_path, mime, resumable=resumable)
+        args = {
+            "body":file_metadata,
+            "media_body":media,
+            "fields":'id'
+        }
+        file = self.service.files(**args).create(**args).execute()
+        return file.get('id',None)
+
+    def create_folder(self, file_metadata:dict[str,Any])->Optional[File]:
+        '''
+        Accessing File in Folder
+        folder_id = '0BwwA4oUTeiV1TGRPeTVjaWRDY1E'
+        file_metadata = {
+            'name': 'photo.jpg',
+            'parents': [folder_id]
+        }
+        '''
+        all_metadata = file_metadata|{'mimeType' : 'application/vnd.google-apps.folder'}
+        file = self.service.files().create(body=all_metadata,fields='id').execute()
+        if file:
+            return file
+        return None
+
+    def update_file_location(self, file_id:str, folder_id:str)->None:
+        # Retrieve the existing parents to remove
+        file = drive_service.files().get(fileId=file_id,fields='parents').execute()
+        previous_parents = ",".join(file.get('parents'))
+        # Move the file to the new folder
+        args = {
+            "fileId":file_id,
+            "addParents":folder_id,
+            "removeParents":previous_parents,
+            "fields":"id, parents",
+        }
+        file = self.service.files().update(**args).execute()
+        return None
+
+    def download(self, file_id:str)->None:
+        request = self.service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
